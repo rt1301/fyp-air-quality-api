8f1a9b4b6ec734c9f4c5d55788170fe1387f393c
Rishabh Taparia <63252510+rt1301@users.noreply.github.com>
2023-05-07 10:44:47 +0530

    Added CORS middleware

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const axios = require("axios");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 const { MongoClient } = require("mongodb");
 
